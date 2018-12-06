09decc05da236989777b818f094940655da57913
BlancaCarrasco <blankcarr@gmail.com>
2018-12-06 07:56:50 -0300

    primert commit proyecto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-/* Acá va tu código */
+
+window.onload = () => {
+  document.getElementById("cifrado").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    let text = document.getElementById('mensajeACifrar').value;
+    let name = document.getElementById('desplazamientos').value;
+    let resultEncode = cipher.encode(text, name)
+    document.getElementById('mostrarMensaje').innerHTML = resultEncode;
+  })
+  document.getElementById("descifrado").addEventListener("click", (evento) => {
+    evento.preventDefault();
+    let text = document.getElementById('mensajeACifrar').value;
+    let name = document.getElementById('desplazamientos').value;
+    let resultDecode = cipher.decode(text, name)
+    document.getElementById('mostrarMensaje').innerHTML = resultDecode;
+  })
+}
+
+
+
+
+
+
+
+//root.innerHTML= cipher.decode(strng, offset)
