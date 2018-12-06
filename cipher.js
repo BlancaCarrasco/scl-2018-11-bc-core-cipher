ae30adefa387faa6debfb9f6caccdaae65d86363
BlancaCarrasco <blankcarr@gmail.com>
2018-12-06 07:56:50 -0300

    primert commit proyecto

--- cipher.js
+++ cipher.js
@@ -1,8 +1,44 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (text, name) => {
+    let resultEncode = "";
+    for (let i = 0; i < text.length; i++) {
+      let mayusculas = text.toUpperCase();
+      let lAscii = mayusculas.charCodeAt(i);
+      if (lAscii === 32) {
+        resultEncode += ' '
+
+      } else {
+        let listo = ((lAscii + 65 + parseInt(name)) % 26) + 65;
+        resultEncode += String.fromCharCode(listo);
+        //console.log(resultEncode);
+        //console.log(text)/* Acá va tu código */
+
+      }
+
+    }
+    return resultEncode;
   },
-  decode: () => {
+
+
+
+
+  decode: (text, name) => {
+    let resultDecode = "";
     /* Acá va tu código */
+    for (let i = 0; i < text.length; i++) {
+      let mayusculas = text.toUpperCase();
+      let lAscii = mayusculas.charCodeAt(i);
+      if (lAscii === 32) {
+        resultDecode += ' '
+      } else {
+        let listo = ((lAscii - 90 - parseInt(name)) % 26) + 90;
+        resultDecode += String.fromCharCode(listo);
+        //console.log(resultDecode)
+
+      }
+
+
+    }
+    return resultDecode;
   }
 };
